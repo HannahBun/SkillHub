8f34de355b5cd9d76716add425f5e85c2bd86528
rojal-011 <rojalnjr@gmail.com>
2025-11-23 14:31:50 -0600

    Update Course.kt

--- SkillHubPro2/app/src/main/java/com/example/skillhub/app/src/main/java/com/example/skillhub/Course.kt
+++ SkillHubPro2/app/src/main/java/com/example/skillhub/app/src/main/java/com/example/skillhub/Course.kt
@@ -1,1 +1,10 @@
+package com.example.skillhub
+
+data class Course(
+    val title: String,
+    val instructorName: String,
+    val price: Int,
+    val timing: String,
+    val rating: Double
+)
 
