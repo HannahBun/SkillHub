d595a29deef802525ff48e1d1d22c9f79a360b3b
rojal-011 <rojalnjr@gmail.com>
2025-11-23 16:15:38 -0600

    Update CourseListActivity.kt

--- SkillHubPro2/app/src/main/java/com/example/skillhub/CourseListActivity.kt
+++ SkillHubPro2/app/src/main/java/com/example/skillhub/CourseListActivity.kt
@@ -9,6 +9,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.app.AlertDialog
+
 
 class CourseListActivity : AppCompatActivity() {
 
